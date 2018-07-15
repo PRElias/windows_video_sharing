c1dd9e20ccbad202f3367d89b58acfff2cd9c3ee
Paulo Roberto Elias <ppuspfc@gmail.com>
2018-07-14 23:35:25 -0300

    Incluindo checagem antes do download

--- executar.ps1
+++ executar.ps1
@@ -1,6 +1,17 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $url = "https://github.com/winnfsd/winnfsd/releases/download/2.4.0/WinNFSd.exe"
 $output = "$PSScriptRoot\WinNFSd.exe"
-Invoke-WebRequest -Uri $url -OutFile $output
+
+if(![System.IO.File]::Exists($output)){
+
+    Write-Host $file "Arquivo não encontrado...baixando."
+    Invoke-WebRequest -Uri $url -OutFile $output
+
+} else {
+
+    Write-Host $file "Arquivo encontrado...pulando download."
+
+}
+
 $videoLibraryPath = $env:userprofile + "\Videos\"
 $PSScriptRoot + "\WinNFSd.exe " + $videoLibraryPath + " /videos"
